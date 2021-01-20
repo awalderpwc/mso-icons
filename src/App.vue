f0fe6e600f83b0d8d65418ec6fd14a759ed10ddd
Alex Walder <alexander.walder@pwc.com>
2021-01-20 14:36:24 +0000

    built

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-primary">
+        <div class="md-toolbar-section-start">
+          <div class="md-title">MSO Icons lookup</div>
+        </div>
+        <md-autocomplete md-layout="box" :md-options="[]" v-model="searchTerm">
+          <label>Search...</label>
+        </md-autocomplete>
+        <div class="md-toolbar-section-end">
+          <div class="md-body">{{ filteredIcons.length }} icons</div>
+        </div>
+      </md-app-toolbar>
+      <md-app-content>
+        <md-empty-state
+          v-if="filteredIcons.length == 0"
+          md-icon="youtube_searched_for"
+          md-label="No icons match your search"
+          md-description="Try searching for another term to find icons that match what you're looking for."
+        >
+        </md-empty-state>
+        <Icons v-else :icons="filteredIcons"></Icons>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ICON_OFFSETS } from "./assets/IconKeys";
+import Icons from "./components/Icons.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Icons },
+  data() {
+    return {
+      iconKeys: ICON_OFFSETS,
+      searchTerm: "",
+    };
+  },
+  computed: {
+    icons() {
+      return this.iconLabels.map((label) => {
+        return {
+          label,
+          offset: this.iconKeys[label],
+        };
+      });
+    },
+    iconLabels() {
+      return Object.keys(this.iconKeys);
+    },
+    filteredIcons() {
+      return this.icons.filter((x) =>
+        x.label.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    },
+  },
+};
 </script>
 
 <style>
+::-webkit-scrollbar {
+  background: transparent;
+  height: 8px;
+  width: 8px;
+}
+::-webkit-scrollbar-thumb {
+  border: none;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  background: #bdc1c6;
+  -webkit-border-radius: 8px;
+  border-radius: 8px;
+  min-height: 40px;
+}
+:hover::-webkit-scrollbar-thumb {
+  background: #dadce0;
+}
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+.md-title {
+  user-select: none;
+}
+.md-app {
+  max-height: 100vh;
+}
+.md-autocomplete {
+  max-width: 600px;
+  margin: 0 auto !important;
 }
 </style>
---
+++ src/components/Icons.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="icons">
+    <div
+      class="icon md-elevation-1"
+      v-for="icon of icons"
+      :key="icon.label"
+      @mouseenter="addShadow($event)"
+      @mouseleave="removeShadow($event)"
+      @click="addToClipboard(icon.label, $event)"
+    >
+      <div
+        class="preview"
+        :style="{ 'background-position': '0 -' + icon.offset + 'px' }"
+      ></div>
+      <div class="label">
+        {{ icon.label }}
+      </div>
+    </div>
+    <md-snackbar
+      md-position="center"
+      :md-active.sync="showSnackbar"
+      md-persistent
+    >
+      <span>"{{ clipboard }}" copied to clipboard!</span
+      ><md-button class="md-primary" @click="showSnackbar = false"
+        >Ok</md-button
+      ></md-snackbar
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Icons",
+  props: {
+    icons: Array,
+  },
+  data() {
+    return {
+      showSnackbar: false,
+      clipboard: null,
+    };
+  },
+  methods: {
+    toggleShadow(elem) {
+      elem.classList.toggle("md-elevation-3");
+    },
+    addShadow(e) {
+      this.toggleShadow(e.target);
+    },
+    removeShadow(e) {
+      this.toggleShadow(e.target);
+    },
+    addToClipboard(text, e) {
+      const elem = e.target;
+      const input = document.createElement("input");
+      elem.appendChild(input);
+      input.value = text;
+      input.select();
+      input.setSelectionRange(0, 99999);
+      document.execCommand("copy");
+      elem.removeChild(input);
+      this.showSnackbar = true;
+      this.clipboard = text;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  user-select: none;
+}
+#icons {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: 100%;
+  max-width: 800px;
+  margin: auto;
+  gap: 10px;
+}
+.icon {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  padding: 10px;
+  box-sizing: border-box;
+}
+.label {
+  font-size: small;
+  word-break: break-all;
+  text-align: center;
+  margin-top: 10px;
+}
+.preview {
+  height: 16px;
+  width: 16px;
+  background: url("https://drive.google.com/uc?id=12jl45AVuFqZkw0eX2Hz9XQKYOXTXuspj");
+  background-repeat: no-repeat;
+}
+input {
+  display: none;
+}
+</style>
